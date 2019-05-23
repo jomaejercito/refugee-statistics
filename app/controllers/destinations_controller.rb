f6e836baa1e453932e7752a44a94c28a59bebb59
Joma Ejercito <jomaejercito@gmail.com>
2019-05-23 02:57:01 -0400

    Add charts to destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,6 @@
 class DestinationsController < ApplicationController
   def index
-    destinations = Destination.select(:name, :population, :id).order(population: :desc).limit(10)
+    destinations = Destination.select(:country_id, :year_id, :population).order(population: :desc).where(year_id: 7).limit(10)
     destination_countries = []
     # Iterate through the list of countries in the database and create an array of hashes that
     # stores the label for each country data plot and its population value.
@@ -8,9 +8,9 @@ class DestinationsController < ApplicationController
     # data plot.
     destinations.each do |destination|
     destination_countries.push({
-        :label => destination.name,
+        :label => destination.country.name,
         :value => destination.population,
-        :link => "destinations/#{destination.id}"
+        :link => "destinations/#{destination.country.id}"
     })
     end
     # Create a new FusionCharts instance that initializes the chart height, width, type, container div
@@ -28,7 +28,7 @@ class DestinationsController < ApplicationController
                 :yAxisName => 'Population',
                 :theme => 'fusion',
             },
-            # The data in the array of hashes is now stored in the `top_ten_populous_countries`
+            # The data in the array of hashes is now stored in the `destination_countries`
             # variable in the FusionCharts consumable format.
             :data => destination_countries
         }
@@ -36,5 +36,29 @@ class DestinationsController < ApplicationController
   end
 
   def show
+    destinations = Destination.select(:country_id, :year_id, :population).where(country_id: params[:id]).limit(7)
+    destination_countries = []
+    destinations.each do |destination|
+    destination_countries.push({
+        :label => destination.year.year,
+        :value => destination.population,
+    })
+    end
+
+    @chart = Fusioncharts::Chart.new({
+        :height => 500,
+        :width => 900,
+        :type => 'line',
+        :renderAt => 'chart-container',
+        :dataSource => {
+            :chart => {
+                :caption => 'Top Countries of Destination for Refugees and Asylum Seekers',
+                :xAxisname => 'Year',
+                :yAxisName => 'Population',
+                :theme => 'ocean',
+            },
+            :data => destination_countries
+        }
+    })    
   end
 end
